74c33845ce6bc7f8cfe9957ae04d9e2ade9ee131
Michael-Xie <xiemicha@gmail.com>
2020-01-20 15:17:08 -0500

    Added styles for different buttons
    Enabled the proper styling and functions of different buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,14 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <>
-      <button>{props.children}</button>
-   </>;
-}
+   let buttonClass = "button";
+ 
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   }
+   if (props.danger) {
+      buttonClass += " button--danger";
+   }
+ 
+   return <button disabled={props.disabled} className={buttonClass} onClick={props.onClick}>{props.children}</button>;
+ }
