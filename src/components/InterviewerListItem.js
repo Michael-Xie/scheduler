946fa8b20bff20800bdcf87b56211ea61079e5cc
Michael-Xie <xiemicha@gmail.com>
2020-01-20 20:54:51 -0500

    Added InterviewerList and InterviewListItem, and associated scss

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classNames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const interviewerClass = {
+    'interviewers__item--selected': props.selected,
+  };
+  return (
+    <li onClick={() => props.setInterviewer(props.id)} className={classNames('interviewers__item', interviewerClass)}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
