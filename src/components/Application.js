34dee43164ac75c12d1f4312dfb38a7aaf39cba9
Michael-Xie <xiemicha@gmail.com>
2020-01-22 15:02:05 -0500

    Used getInterview selector in Application to return accepted interview object format

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,42 +4,43 @@ import "components/Application.scss";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index";
 import axios from "axios";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Grace Louis",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png"
-      }
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Grace Louis",
+//       interviewer: {
+//         id: 2,
+//         name: "Tori Malcolm",
+//         avatar: "https://i.imgur.com/Nmx0Qxo.png"
+//       }
 
-    }
-  }
-];
+//     }
+//   }
+// ];
 
 export default function Application(props) {
   const initialState = {
@@ -60,7 +61,7 @@ export default function Application(props) {
       console.log("days", days);
       console.log("appointments", appointments);
       console.log("interviewers", interviewers);
-      setState(prev => ({ days: days.data, appointments: appointments.data }));
+      setState(prev => ({ days: days.data, appointments: appointments.data, interviewers: interviewers.data }));
     })
   }, []); // run once with the [], else none would run each render
 
@@ -92,13 +93,21 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */
-        appointments.map((appointment) => {
-          return (
-            <Appointment key={appointment.id} {...appointment}/>
-          )
-        })}
-          <Appointment key="last" time="5pm" />
-        
+          
+          getAppointmentsForDay(state, state.day).map((appointment) => {
+            const interview = getInterview(state, appointment.interview);
+
+            return (
+              <Appointment
+                key={appointment.id}
+                id={appointment.id}
+                time={appointment.time}
+                interview={interview}
+              />
+            )
+          })}
+
+
       </section>
     </main>
   );
