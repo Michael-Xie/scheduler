4e5fd0f326e1be5afd3eca16b41f1dc9a7772722
Michael-Xie <xiemicha@gmail.com>
2020-01-22 16:30:00 -0500

    Enabled useVisualMode to go back to previous mode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,11 +2,21 @@ import React, {useState} from "react";
 
 export default function useVisualMode(initialMode) {
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] = useState([initialMode] || []);
   const transition = (currMode) => {
     setMode(currMode);
+    setHistory([...history, currMode])
   };
+  const back = () => {
+    if(history.length > 1) {
+      history.pop();
+    }
+    const lastMode = history[history.length - 1];
+    setMode(lastMode);
+  }
   return {
     mode,
-    transition
+    transition,
+    back
   };
 }
