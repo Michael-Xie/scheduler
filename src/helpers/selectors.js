16c65248149ce6c24f638b1759d07ad92adf2a83
Michael-Xie <xiemicha@gmail.com>
2020-01-22 00:08:52 -0500

    Added getInterview to selectors helper and unit tested

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,9 +1,20 @@
 export function getAppointmentsForDay(state, day) {
   let selectedDayAppt = []
   const filteredDays = state.days.filter(currDay => currDay.name === day);
+  // there can only be one element 
   if (filteredDays[0]) {
     selectedDayAppt = filteredDays[0].appointments.map((value)=>state.appointments[value]);
   }
-  console.log("appointment", selectedDayAppt);
   return selectedDayAppt;
+}
+
+export function getInterview(state, interview) {
+  let interviewObj = null;
+  if (interview) {
+    const studentName = interview.student;
+    const interviewer = state.interviewers[interview.interviewer];
+    interviewObj = {student: studentName, interviewer}
+  }
+
+  return interviewObj;
 }
